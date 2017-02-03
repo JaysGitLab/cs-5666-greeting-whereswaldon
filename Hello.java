ab8aba309a045c38ab3dc0611d4e46f2fc55e295
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-02-03 10:42:58 -0500

    Implement named user feature

--- Hello.java
+++ Hello.java
@@ -1,6 +1,14 @@
 public class Hello {
 	public static void main(String[] args) {
-		print();
+		if (args.length > 1) {
+			print(args[1]);
+		} else {
+			print();
+		}
+	}
+
+	public static void print(String name) {
+		System.out.println("Hello, "+name+"!");
 	}
 
 	public static void print() {
