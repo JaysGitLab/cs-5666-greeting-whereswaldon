b659a1450620ab0dcac4aa7e7c53b8d08477d4e1
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-02-05 13:41:30 -0500

    Implement -me flag feature and document

--- Hello.java
+++ Hello.java
@@ -1,18 +1,60 @@
 public class Hello {
+    	private static String name = "Jeeves";
+    	private static String friend = "";
+
 	public static void main(String[] args) {
+		setArgs(args);
+                print(friend);
+		introduce();
+	}
+
+        /**
+        setArgs parses the command line arguments to set global state.
+        */
+	private static void setArgs(String[] args) {
+    		name = "Jeeves";
+    		friend = "";
 		if (args.length > 0) {
-			print(args[0]);
-		} else {
-			print();
+            		for (int i = 0; i < args.length; i++) {
+                		String arg = args[i];
+                		if (arg.startsWith("-me")) {
+                                        if (arg.length() > 3) {
+                                                name = arg.substring(3);
+                                        } else {
+                                                name = args[i+1];
+                                                i++;
+                                        }
+                		} else {
+                    			friend = arg;
+                		}
+        		}
 		}
 	}
 
-	public static void print(String name) {
-    		String first = ("" + name.charAt(0)).toUpperCase();
-		System.out.println("Hello, "+first + name.substring(1)+"!");
+        /**
+        introduce prints an introduction from the program.
+        */
+	private static void introduce() {
+    		System.out.println("I am "+name+".");
 	}
 
-	public static void print() {
-		System.out.println("Hello, Cthulhu!");
+        /**
+        capitalize ensures that the first letter of a string is capitalized.
+        */
+	private static String capitalize(String name) {
+    		return name.charAt(0) < 'A' || name.charAt(0) > 'Z'
+        		? ("" + name.charAt(0)).toUpperCase() + name.substring(1)
+        		: name;
+	}
+
+	/**
+	print greets the user with the provided name.
+	*/
+	public static void print(String name) {
+    		if (! name.equals("")) {
+        		System.out.println("Hello, "+capitalize(name)+"!");
+    		} else {
+        		System.out.println("Hello, Cthulhu!");
+    		}
 	}
 }
